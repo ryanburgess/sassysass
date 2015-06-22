6a8cb42b26c52a816c489f3b4f3dc2148af2d955
ryanburgess <ryanb@netflix.com>
2015-06-22 15:54:10 -0700

    remove nodeunit test for now

--- gruntfile.js
+++ gruntfile.js
@@ -11,5 +11,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  grunt.registerTask('test', ['jshint', 'nodeunit']);
+  grunt.registerTask('test', ['jshint'/*, 'nodeunit'*/]);
 };
