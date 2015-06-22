87989db72f1cfdbee7a261555c47d77da28f9393
ryanburgess <ryanb@netflix.com>
2015-06-22 12:50:02 -0700

    add jsonlint to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp        = require('gulp'),
+  jsonlint      = require("gulp-jsonlint"),
   jshint        = require('gulp-jshint'),
   jshintStyle   = require('jshint-stylish'),
   path          = require('path');
 
+// JS Hint
 gulp.task('jshint', function() {
   return gulp.src('./bin/*')
     .pipe(jshint())
@@ -10,8 +12,16 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('fail'));
 });
 
+// JSON Lint
+gulp.task('jsonlint', function() {
+  return gulp.src(['./templates/json/*.json', 'package.json'])
+    .pipe(jsonlint())
+    .pipe(jsonlint.reporter(jshintStyle));
+});
+
 gulp.task('watch', function() {
   gulp.watch('bin/*', ['jshint']);
+  gulp.watch(['./templates/json/*.json', 'package.json'], ['jsonlint']);
 });
 
 gulp.task('default', ['watch']);
