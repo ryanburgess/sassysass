19c12b7c540a549eaac2a0a963f448e5aaa89c94
ryanburgess <ryanb@netflix.com>
2015-06-23 18:15:30 -0700

    use gulp for tests instead of grunt

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp'),
   jsonlint      = require("gulp-jsonlint"),
   jshint        = require('gulp-jshint'),
+  nodeunit      = require('gulp-nodeunit'),
   jshintStyle   = require('jshint-stylish'),
   path          = require('path');
 
@@ -19,9 +20,22 @@ gulp.task('jsonlint', function() {
     .pipe(jsonlint.reporter(jshintStyle));
 });
 
+// watch tasks
 gulp.task('watch', function() {
   gulp.watch('bin/*', ['jshint']);
   gulp.watch(['./templates/json/*.json', 'package.json'], ['jsonlint']);
 });
 
+// nodeunit tests
+gulp.task('nodeunit', function () {
+  gulp.src('tests/**/*.js')
+    .pipe(nodeunit({
+      reporter: 'junit',
+      reporterOptions: {
+        output: 'test-output'
+      }
+    }));
+});
+
+gulp.task('test', ['jsonlint', 'jshint', 'nodeunit']);
 gulp.task('default', ['watch']);
