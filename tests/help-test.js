0a51bb110bcb3ea2c9362d3a9f15a08667c78e5c
ryanburgess <ryanb@netflix.com>
2015-12-05 16:35:50 -0800

    add eslint rules and clean up js errors

--- tests/help-test.js
+++ tests/help-test.js
@@ -1,7 +1,7 @@
 //this test causing an error
 
 // var exec = require('child_process').exec;
-// exports.helpReturns = function(test){
+// exports.helpReturns = function(test) {
 //   exec(['sassysass help'], function (error, stdout, stderr) {
 //     test.notEqual(stdout, '');
 //     test.done();
