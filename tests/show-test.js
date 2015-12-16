66f98eb47ed2a9aa5bf5271260e46a1443dcc9eb
ryanburgess <ryanb@netflix.com>
2015-12-16 08:36:22 -0800

    update show test

--- tests/show-test.js
+++ tests/show-test.js
@@ -3,6 +3,7 @@ var test = require('tape');
 var show = require('../lib/commands/show');
 
 test('Test show function', function (t) {
+  t.equal(show(), undefined, 'Not a command. Try sassysass show <modules, themes, layout, mixins>');
   t.equal(show('modules'), undefined, 'Pass - Show modules working');
   t.equal(show('mixins'), undefined, 'Pass - Show mixins working');
   t.equal(show('layout'), undefined, 'Pass - Show layout working');
