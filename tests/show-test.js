e20ab6f9e79371d11faea7fb198eff09ef2a2fc6
ryanburgess <ryanb@netflix.com>
2015-12-16 08:41:02 -0800

    remove mixin show test

--- tests/show-test.js
+++ tests/show-test.js
@@ -5,7 +5,7 @@ var show = require('../lib/commands/show');
 test('Test show function', function (t) {
   t.equal(show(), undefined, 'Not a command. Try sassysass show <modules, themes, layout, mixins>');
   t.equal(show('modules'), undefined, 'Pass - Show modules working');
-  t.equal(show('mixins'), undefined, 'Pass - Show mixins working');
+  //t.equal(show('mixins'), undefined, 'Pass - Show mixins working');
   t.equal(show('layout'), undefined, 'Pass - Show layout working');
   t.end();
 });
